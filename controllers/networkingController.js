3823989a2dd2ddf9233d806574dde8fe5d5c1228
Nick Kramer <krames12@gmail.com>
2017-02-02 10:05:42 -0500

    get, post and delete routes added

--- controllers/networkingController.js
+++ controllers/networkingController.js
@@ -15,4 +15,26 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app) {
 
+  // initial page load request
+  app.get('/people', (req, res) => {
+    Person.find({}, (error, data) => {
+      if (error) throw error;
+      console.log(data);
+      res.render('index', { people: data });
+    });
+  });
+
+  app.post('/people', urlencodedParser, (req, res) => {
+    var newPerson = Person(req.body).save((error, data) => {
+      if (error) throw error;
+      res.json(data);
+    });
+  });
+
+  app.delete('/person/:item', (req, res) => {
+    Person.find({item: req.params.item.replace(/\-/g, " ")}).remove( (error, data) => {
+      if (error) throw error;
+      res.json(data);
+    });
+  });
 };
