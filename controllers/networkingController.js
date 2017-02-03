d3725a600edbca9bed5102d75197364c3fdea89c
Nick Kramer <krames12@gmail.com>
2017-02-03 09:24:55 -0500

    now able to add people to server

--- controllers/networkingController.js
+++ controllers/networkingController.js
@@ -11,6 +11,11 @@ var personSchema = new mongoose.Schema({
 
 var Person = mongoose.model('Person', personSchema);
 
+// var personOne = Person({name: "nick", interests: "javascript"}).save((error, data) => {
+//   if (error) throw error;
+//   console.log("person saved");
+// });
+
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app) {
@@ -20,13 +25,13 @@ module.exports = function(app) {
     console.log('get request initated');
     Person.find({}, (error, data) => {
       if (error) throw error;
-      console.log("get data:", data);
       res.render('index', { people: data });
     });
   });
 
   app.post('/people', urlencodedParser, (req, res) => {
     console.log('post request initiated');
+    console.log('req body:', req.body);
     var newPerson = Person(req.body).save((error, data) => {
       if (error) throw error;
       console.log("post data:", data);
@@ -35,7 +40,7 @@ module.exports = function(app) {
   });
 
   app.delete('/people/:item', (req, res) => {
-    Person.find({item: req.params.item.replace(/\-/g, " ")}).remove( (error, data) => {
+    Person.find({person: req.params.item.replace(/\-/g, " ")}).remove( (error, data) => {
       if (error) throw error;
       res.json(data);
     });
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,25 +1,39 @@
 $(document).ready(() => {
 
-  $('person-form').on('submit', function() {
+  $('form').on('submit', function() {
 
-    var name = $('person-name');
-    var interests = $('person-interests');
+    var name = $('#person-name');
+    var interests = $('#person-interests');
     var person = {
       name: name.val(),
       interests: interests.val()
     }
+
+    console.log("person", person);
 
     $.ajax({
       type: 'POST',
       url: '/people',
       data: person,
       success: function(data) {
+        console.log("data", data);
         location.reload();
       }
     });
 
     return false;
 
+  });
+
+  $('li').on('click', function() {
+    var item = $(this).text().replace(/ /g, "-");
+    $.ajax({
+      type: 'DELETE',
+      url: '/todo/' + item,
+      success: function(data){
+        location.reload();
+      }
+    });
   });
 
 });
